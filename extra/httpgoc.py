daf9e689872b2e5586eec22273b15ce68d7a66da
Lojacopsen <68278515+Lojacops@users.noreply.github.com>
2021-10-24 21:36:47 +0200

    Update httpgoc.py

--- extra/httpgoc.py
+++ extra/httpgoc.py
@@ -1,4 +1,3 @@
-#Importing the modules
 import socket
 import sys
 
@@ -13,23 +12,19 @@ def serop():
     server_address = ("localhost", port)
     print("Server started")
     sock.bind(server_address)
-    
-    # Listen for coming connections
+
     sock.listen(1)
-    
-    #A loop waiting for connection
+
     while True :
-        # Wait for a connection
         print ("Waiting for a connection")
         connection, client_address = sock.accept()
         try:
-           client_address = str(client_address)
-           print("We got a connection from" + client_address)
-
-          while True:
-               data = connection.recv(16)
-               data = str(data)
-               print ("received \n "+ data)
+            client_address = str(client_address)
+            print("We got a connection from" + client_address)
+            while True:
+                data = connection.recv(16)
+                data = str(data)
+                print ("received \n "+ data)
                 if data:
                     print ("sending data back to the client")
                     connection.send(b'Hello Client !!')
@@ -38,6 +33,4 @@ def serop():
                     break
                 
         finally:
-        # Closing the connection
-       #connection.close()
             print("Done")
