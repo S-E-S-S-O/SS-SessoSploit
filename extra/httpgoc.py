079601d95a94495f790352bb18d8a2884786b17e
Takaso <70639611+Takaso@users.noreply.github.com>
2021-11-06 15:25:47 +0100

    Update httpgoc.py

--- extra/httpgoc.py
+++ extra/httpgoc.py
@@ -1,36 +1,9 @@
-import socket
-import sys
+import http.server as htpsrvr
+import socketserver
 
-def serop():
-    #Taking some input
-    port = input("Please tell the port you want to listen on \n")
-    port = int(port)
-
-    #Making the socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    server_address = ("localhost", port)
-    print("Server started")
-    sock.bind(server_address)
-
-    sock.listen(1)
-
-    while True :
-        print ("Waiting for a connection")
-        connection, client_address = sock.accept()
-        try:
-            client_address = str(client_address)
-            print("We got a connection from" + client_address)
-            while True:
-                data = connection.recv(16)
-                data = str(data)
-                print ("received \n "+ data)
-                if data:
-                    print ("sending data back to the client")
-                    connection.send(b'Hello Client !!')
-                else:
-                    print("no more data from" + client_address)
-                    break
-                
-        finally:
-            print("Done")
+def run_http_server():
+    PORT = 8080
+    handler = htpsrvr.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", PORT), handler) as httpd:
+      httpd.serve_forever()
+    print(f"Serving HTTP at {PORT} \n")
